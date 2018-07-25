04e0cef04e86ee7cbe6631c0b24f19076952c244
David Folks <aldwyn.wot@gmail.com>
2018-07-25 12:38:23 -0600

    saved changes

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
-  api.updateItem()
+  api.updateItem();
   api.getItems((items) => {
     items.forEach((item) => store.addItem(item));  
     shoppingList.render();
--- scripts/store.js
+++ scripts/store.js
@@ -15,24 +15,24 @@ const store = (function(){
     const item = this.findById(id);
     Object.assign(item, newData);
   };
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
+  // const findAndToggleChecked = function(id) {
+  //   const item = this.findById(id);
+  //   item.checked = !item.checked;
+  // };
 
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
   };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
-  };
+  // const findAndUpdateName = function(id, name) {
+  //   try {
+  //     Item.validateName(name);
+  //     const item = this.findById(id);
+  //     item.name = name;
+  //   } catch(e) {
+  //     console.log('Cannot update name: ' + e.message);
+  //   }
+  // };
 
   const toggleCheckedFilter = function() {
     this.hideCheckedItems = !this.hideCheckedItems;
@@ -51,9 +51,7 @@ const store = (function(){
     findById,
     findAndDelete,
     toggleCheckedFilter,
-    setSearchTerm,
-    findAndToggleChecked,
-    findAndUpdateName
+    setSearchTerm,  
   };
   
 }());
